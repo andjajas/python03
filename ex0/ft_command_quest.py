168724a2fe937e5a4f2c71a4435c776f30ad584f
Andre Djajasaputra <andjajas@f0r1s14.codam.nl>
2026-07-18 18:08:30 +0200

    ex0 done

---
+++ ex0/ft_command_quest.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import sys
+
+if __name__ == "__main__":
+    print("=== Command Quest ===")
+    if len(sys.argv) == 1:
+        print(f"Program name: {sys.argv[0]}")
+        print("No arguments provided!")
+        print("Total arguments: 1")
+    else:
+        print(f"Program name: {sys.argv[0]}")
+        print(f"Arguments received: {len(sys.argv[1:])}")
+        i = 1
+        while i < len(sys.argv):
+            print(f"Argument {i}: {sys.argv[i]}")
+            i += 1
+        print(f"Total arguments: {len(sys.argv)}")
