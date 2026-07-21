2aa98c479c3047a27a709b6e7d00439470e08cb6
Andre Djajasaputra <andjajas@f1r6s8.codam.nl>
2026-07-21 17:24:55 +0200

    messing with ex1

--- ex1/ft_score_analytics.py
+++ ex1/ft_score_analytics.py
@@ -1,5 +1,29 @@
 #!/usr/bin/env python3
 import sys
-while i < len(sys.argv[1:])
 scores: list[int] = []
-for arg in sys.argv[1:]
+for arg in sys.argv[1:]:
+    try:
+        scores += [int(arg)]
+    except Exception as e:
+        print("Caught arg error", e)
+players: int = len(scores)
+print(f"Total players: {players}")
+tot_score: int = 0
+for score in scores[0:]:
+    tot_score += score
+print(f"Total score: {tot_score}")
+if players > 0:
+    avg_score: float = tot_score / players
+    print(f"Average score: {avg_score:0.1f}")
+    high_score = max(scores)
+    print(f"High score: {high_score}")
+    low_score = min(scores)
+    print(f"Low score: {low_score}")
+    sc_range = high_score - low_score
+    print(f"Score range: {sc_range}")
+else:
+    print(
+        f"No scores provided. Usage: python3 "
+        f"ft_score_analytics.py <score1> <score2> ..."
+     )
+
