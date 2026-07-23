60fde0e0d7b82986d9bcdf14048bdf1e583ef042
Andre Djajasaputra <andjajas@f1r2s15.codam.nl>
2026-07-23 18:03:00 +0200

    removed alt fixed ex1

--- ex1/ft_score_analytics.py
+++ ex1/ft_score_analytics.py
@@ -1,24 +1,28 @@
 #!/usr/bin/env python3
 import sys
 
+class NegativeNumber(ValueError):
+    pass
+
 def score_analytics():
     print("=== Player Score Analytics ===")
     scores: list[int] = []
     for arg in sys.argv[1:]:
         try:
-            scores += [int(arg)]
+            temp = int(arg)
+            if temp < 0:
+                raise NegativeNumber(
+                    f"Invalid parameter: '{int(arg)}', "
+                    f"Value cannot be negative"
+                 )
+            scores += [temp]
+        except NegativeNumber as e:
+            print(e)
         except ValueError:
             print(f"Invalid parameter: '{arg}'")
-    if arg <= 0:
-        try:
-            scores += [int(arg)]
-            if 
-
-        except ValueError:
-            print(f"Invalid parameter: ' '{arg}', Value has to be more than 0")
-    print(f"Scores processed: {scores}")
     players: int = len(scores)
     if players > 0:
+        print(f"Scores processed: {scores}")
         print(f"Total players: {players}")
         tot_score: int = 0
         for score in scores[0:]:
@@ -37,7 +41,6 @@ def score_analytics():
             f"No scores provided. Usage: python3 "
             f"ft_score_analytics.py <score1> <score2> ..."
         )
-
 
 if __name__ == "__main__":
     score_analytics()
