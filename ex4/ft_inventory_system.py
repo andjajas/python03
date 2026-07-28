344aa219921dfdc9358a555ef9582466b4901199
Andre Djajasaputra <andjajas@f1r2s10.codam.nl>
2026-07-28 19:48:51 +0200

    start ex4

--- ex4/ft_inventory_system.py
+++ ex4/ft_inventory_system.py
@@ -1,2 +1,32 @@
 #!/usr/bin/env python3
 import sys
+inventory = {}
+for i in sys.argv[1:]:
+    if ":" in i:
+        try:
+            key, value = i.split(":", 1)
+            inventory[key] = value
+        except ValueError:
+            print(f"Redundant item '{key}' - discarding")
+    else:
+        print(f"Error - invalid parameter '{i}")
+
+
+# inventory.update({
+#     "sword":1,
+#     "potion":5,
+#     "shield":2,
+#     "armor":3,
+#     "helmet":1,
+#     # "sword":2,
+#     # "hello",
+#     # "key":value
+# })
+
+
+def ft_show_dict() -> None:
+    print(f"{inventory}")
+
+
+if __name__ == "__main__":
+    ft_show_dict()
