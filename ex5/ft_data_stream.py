77b9decb931427c58bb00612a9b1fc474792cbea
Andre Djajasaputra <andjajas@f1r6s2.codam.nl>
2026-07-31 14:56:18 +0200

    ex5 done

--- ex5/ft_data_stream.py
+++ ex5/ft_data_stream.py
@@ -1,11 +1,44 @@
 #!/usr/bin/env python3
-def gen_event() -> tuple[str, str]:
-    pass
+import typing
+import random
 
 
-def consume_event() -> None:
-    pass
+players_list: list[str] = [
+    'alice', 'bob', 'charlie', 'dylan']
+
+
+actions_list: list[str] = [
+    'climb', 'eat', 'grab', 'move', 'release', 'run', 'sleep',
+    'swim', 'use']
+
+
+def gen_event() -> typing.Generator[tuple[str, str], None, None]:
+    while True:
+        name = random.choice(players_list)
+        action = random.choice(actions_list)
+        yield (name, action)
+
+
+def consume_event(
+        event_list: list[tuple[str, str]]
+        ) -> typing.Generator[tuple[str, str], None, None]:
+    while event_list:
+        idx = random.randrange(len(event_list))
+        yield (event_list[idx])
+        event_list[idx:idx+1] = []
 
 
 if __name__ == "__main__":
-    gen_event()
+    gen = gen_event()
+    for i in range(1000):
+        name, action = next(gen)
+        print(f"Event {i}: Player {name} did action {action}")
+    event_list: list[tuple[str, str]] = []
+    for i in range(10):
+        event: tuple[str, str] = next(gen)
+        event_list += [event]
+    print(f"Built list of {len(event_list)} events: {event_list}")
+    for event in consume_event(event_list):
+        print(f"Got event from list: {event}")
+        print(f"Remains in list: {event_list}")
+    print(f"Remains in list: {event_list}")
